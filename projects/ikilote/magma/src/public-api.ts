49f043f0c599d3b5772ffbbe48ac9dcd2acb11e8
Zéfling <zefling@ikilote.net>
2024-12-27 02:55:17 +0100

    Initial commit

---
+++ projects/ikilote/magma/src/public-api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of magma
+ */
+
+export * from './lib/components/context-menu/context-menu.component';
+export * from './lib/components/context-menu/context-menu.directive';
+export * from './lib/components/dialog/dialog.component';
+export * from './lib/components/tabs/tab-content.component';
+export * from './lib/components/tabs/tab-title.component';
+export * from './lib/components/tabs/tabs.component';
+

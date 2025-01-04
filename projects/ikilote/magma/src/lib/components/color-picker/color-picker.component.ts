95599df7247e2457e990972ed9032adfb5c55d6a
Zéfling <zefling@ikilote.net>
2025-01-04 18:10:47 +0100

    Update color on click in SL zone

--- projects/ikilote/magma/src/lib/components/color-picker/color-picker.component.ts
+++ projects/ikilote/magma/src/lib/components/color-picker/color-picker.component.ts
@@ -25,24 +25,25 @@ import Color from 'colorjs.io';
     },
 })
 export class ColorPickerComponent implements OnInit, OnChanges, AfterViewInit {
-    zone = viewChild.required<ElementRef<HTMLDivElement>>('cursorZone');
-    drag = viewChild.required(CdkDrag);
+    readonly zone = viewChild.required<ElementRef<HTMLDivElement>>('cursorZone');
+    readonly drag = viewChild.required(CdkDrag);
 
-    color = input('#d94040');
+    readonly color = input('#d94040');
 
-    update = output<string>();
+    readonly update = output<string>();
 
-    rangeHue = 0;
-    rangeAlpha = 1;
-    rangeLight = 0;
-    rangeSature = 0;
+    protected rangeHue = 0;
+    protected rangeAlpha = 1;
+    protected rangeLight = 0;
+    protected rangeSature = 0;
 
-    hslac = '';
-    hsla = '';
-    rgba = '';
-    hexa = '';
+    protected hsla = '';
+    protected rgba = '';
+    protected hexa = '';
 
-    pos: Point = { x: 0, y: 0 };
+    protected pos: Point = { x: 0, y: 0 };
+
+    private startDrag = false;
 
     ngOnInit(): void {
         console.log('ngOnInit');
@@ -56,23 +57,12 @@ export class ColorPickerComponent implements OnInit, OnChanges, AfterViewInit {
 
     ngAfterViewInit(): void {
         setTimeout(() => {
-            const color = new Color(this.color());
-            const hls = color.toGamut({ space: 'hsl' }).to('hsl');
-
-            // value calculation
-            this.rangeHue = hls.h;
-            this.rangeAlpha = hls.alpha;
-            this.rangeSature = 100 - hls.s;
-            this.rangeLight = 100 - (100 * hls.l) / (100 - hls.s / 2);
-
-            // position calculation
-            const { clientWidth, clientHeight } = this.zone().nativeElement;
-            this.pos = {
-                x: ((clientWidth - 10) * this.rangeLight) / 100,
-                y: ((clientHeight - 10) * this.rangeSature) / 100,
-            };
-            this.updateColor();
+            this.updateWithHLS(new Color(this.color()));
         }, 100);
+    }
+
+    dragStart() {
+        this.startDrag = true;
     }
 
     dragEnd(event: CdkDragEnd<any>) {
@@ -81,9 +71,49 @@ export class ColorPickerComponent implements OnInit, OnChanges, AfterViewInit {
         this.rangeLight = Math.round((x / (clientWidth - 10)) * 100);
         this.rangeSature = Math.round((y / (clientHeight - 10)) * 100);
         this.updateColor();
+        setTimeout(() => {
+            this.startDrag = false;
+        }, 10);
     }
 
-    updateColor() {
+    updateHex(value: string) {
+        try {
+            this.updateWithHLS(new Color(value));
+        } catch (e) {
+            console.log('Invalid color');
+        }
+    }
+
+    protected click(event: MouseEvent) {
+        if (!this.startDrag) {
+            const { layerX, layerY } = event;
+            const { clientWidth, clientHeight } = this.zone().nativeElement;
+            this.rangeLight = Math.round((layerX / (clientWidth - 10)) * 100);
+            this.rangeSature = Math.round((layerY / (clientHeight - 10)) * 100);
+            this.drag().setFreeDragPosition({ x: layerX - 5, y: layerY - 5 });
+            this.updateColor();
+        }
+    }
+
+    private updateWithHLS(color: Color) {
+        const hls = color.toGamut({ space: 'hsl' }).to('hsl');
+
+        // value calculation
+        this.rangeHue = hls.h;
+        this.rangeAlpha = hls.alpha;
+        this.rangeSature = 100 - hls.s;
+        this.rangeLight = 100 - (100 * hls.l) / (100 - hls.s / 2);
+
+        // position calculation
+        const { clientWidth, clientHeight } = this.zone().nativeElement;
+        this.pos = {
+            x: ((clientWidth - 10) * this.rangeLight) / 100,
+            y: ((clientHeight - 10) * this.rangeSature) / 100,
+        };
+        this.updateColor();
+    }
+
+    protected updateColor() {
         const color = new Color(
             'hsl',
             [

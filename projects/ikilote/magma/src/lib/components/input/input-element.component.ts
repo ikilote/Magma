37c7fe5827e787df7f9784fee526bdf13b747dc7
Zéfling <zefling@ikilote.net>
2025-01-17 23:28:25 +0100

    Add affix support

--- projects/ikilote/magma/src/lib/components/input/input-element.component.ts
+++ projects/ikilote/magma/src/lib/components/input/input-element.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector: 'mg-input-label, mg-input-desc',
+    selector: 'mg-input-label, mg-input-desc, mg-input-prefix, mg-input-suffix',
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: '<ng-content></ng-content>',
+    styleUrls: ['input-element.component.scss'],
 })
 export class MagmaInputElement {}

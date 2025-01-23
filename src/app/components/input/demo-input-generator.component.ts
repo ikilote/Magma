d315fe62b7559eb5a7a4e81f3d454f2e2b9c5028
Zéfling <zefling@ikilote.net>
2025-01-23 01:21:33 +0100

    Generator: radio & checkbox multiples

--- src/app/components/input/demo-input-generator.component.ts
+++ src/app/components/input/demo-input-generator.component.ts
@@ -48,7 +48,12 @@ export class DemoInputGeneratorComponent {
         type: FormControl<string>;
         prefix: FormControl<string>;
         suffix: FormControl<string>;
-        subLabel: FormControl<string>;
+        subLabel1: FormControl<string>;
+        subValue1: FormControl<string>;
+        subLabel2: FormControl<string>;
+        subValue2: FormControl<string>;
+        subLabel3: FormControl<string>;
+        subValue3: FormControl<string>;
     }>;
 
     codeHtml = '';
@@ -60,7 +65,12 @@ export class DemoInputGeneratorComponent {
             desc: { default: '' },
             prefix: { default: '' },
             suffix: { default: '' },
-            subLabel: { default: 'input label' },
+            subLabel1: { default: 'input label 1' },
+            subValue1: { default: 'value1' },
+            subLabel2: { default: 'input label 2' },
+            subValue2: { default: 'value2' },
+            subLabel3: { default: 'input label 3' },
+            subValue3: { default: 'value3' },
         });
         this.codeGeneration();
         this.formGenerator.valueChanges.subscribe(() => {
@@ -95,22 +105,31 @@ export class DemoInputGeneratorComponent {
             body.push(jsonLabel);
         }
         if (this.formGenerator.value.type) {
-            const bodyInput: string[] = [];
-            const attrInput: Json2htmlAttr = {};
-            const jsonInput: Json2htmlRef = {
-                tag: 'mg-input-' + this.formGenerator.value.type,
-                attrs: attrInput,
-                body: bodyInput,
-            };
+            this.addType(
+                body,
+                this.formGenerator.value.type,
+                this.formGenerator.value.subLabel1,
+                this.formGenerator.value.subValue1,
+            );
 
             if (
                 (this.formGenerator.value.type === 'radio' || this.formGenerator.value.type === 'checkbox') &&
-                this.formGenerator.value.subLabel
+                this.formGenerator.value.subLabel1
             ) {
-                bodyInput.push(this.formGenerator.value.subLabel);
-            }
+                this.addType(
+                    body,
+                    this.formGenerator.value.type,
+                    this.formGenerator.value.subLabel2,
+                    this.formGenerator.value.subValue2,
+                );
 
-            body.push(jsonInput);
+                this.addType(
+                    body,
+                    this.formGenerator.value.type,
+                    this.formGenerator.value.subLabel3,
+                    this.formGenerator.value.subValue3,
+                );
+            }
         }
 
         if (this.formGenerator.value.label) {
@@ -136,5 +155,23 @@ export class DemoInputGeneratorComponent {
         }
 
         this.codeHtml = new Json2html(json).toString();
+    }
+
+    addType(body: Json2htmlRef[], type: string, label?: string, value?: string) {
+        const bodyInput: string[] = [];
+        const attrInput: Json2htmlAttr = {};
+        const jsonInput: Json2htmlRef = {
+            tag: 'mg-input-' + type,
+            attrs: attrInput,
+            body: bodyInput,
+        };
+
+        if ((type === 'radio' || type === 'checkbox') && (value || label)) {
+            if (label) {
+                bodyInput.push(label);
+            }
+            attrInput['value'] = value;
+        }
+        body.push(jsonInput);
     }
 }

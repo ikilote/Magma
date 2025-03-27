5dbfe47e2c12e3c9e84590c3aa30a7b4dcbc594d
Zéfling <zefling@ikilote.net>
2025-03-27 22:55:26 +0100

    Add jsonParse util (with Bowser)

--- projects/ikilote/magma/src/lib/utils/json.ts
+++ projects/ikilote/magma/src/lib/utils/json.ts
@@ -1,3 +1,63 @@
+import Bowser from 'bowser';
+
 export function jsonCopy<T>(o: T): T {
     return JSON.parse(JSON.stringify(o));
+}
+
+export class ExceptionJsonParse extends Error {
+    override cause: string;
+    constructor(message: string, cause: string) {
+        super(message); // 'Error' breaks prototype chain here
+        Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+        this.name = 'ExceptionJsonParse';
+        this.cause = cause;
+    }
+}
+
+/**
+ * parse JSON
+ * @param value parse string
+ * @returns json
+ * @throw SyntaxError with position formatted in cause
+ */
+export function jsonParse<T>(value: string): T | undefined {
+    try {
+        return JSON.parse(value);
+    } catch (error) {
+        const message = (error as any).message.match(/[^\n]+/)[0];
+        let textPosition = '';
+        if (error instanceof SyntaxError) {
+            const browser = Bowser.parse(window.navigator.userAgent);
+            if (browser.engine.name === 'Blink') {
+                if (message.match(/at position/)) {
+                    const position = parseInt(message.match(/at position (\d+)/)[1], 10);
+                    const lines = value.split(/\n/);
+                    let l = 1;
+                    for (const line of lines) {
+                        if (line) {
+                            if (l + line.length >= position) {
+                                textPosition = line + '\n' + ' '.repeat(position - l + 1) + '^';
+                                break;
+                            }
+                            l += line.length + 1;
+                        }
+                    }
+                }
+            } else if (browser.engine.name === 'Gecko') {
+                if (message.match(/at line/)) {
+                    const [, line, column] = message.match(/at line (\d+) column (\d+)/);
+                    const lines = value.split(/\n/);
+                    if (lines[+line - 1]) {
+                        textPosition = lines[+line - 1] + '\n' + ' '.repeat(+column - 1) + '^';
+                    }
+                }
+            } else if (browser.engine.name === 'WebKit') {
+                const [, pos] = message.match(/.*JSON Parse error: (.*)/);
+                if (pos) {
+                    textPosition = pos;
+                }
+            }
+        }
+        throw new ExceptionJsonParse(message, textPosition);
+    }
 }
--- src/app/utils/json/demo-json.component.ts
+++ src/app/utils/json/demo-json.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import {
+    ExceptionJsonParse,
+    MagmaInput,
+    MagmaInputElement,
+    MagmaInputTextarea,
+    jsonParse,
+} from '../../../../projects/ikilote/magma/src/public-api';
 import { CodeTabsComponent } from '../../demo/code-tabs.component';
 
 @Component({
     selector: 'demo-json',
     templateUrl: './demo-json.component.html',
     styleUrls: ['./demo-json.component.scss'],
-    imports: [CodeTabsComponent],
+    imports: [CodeTabsComponent, MagmaInput, MagmaInputTextarea, FormsModule, MagmaInputElement],
 })
 export class DemoJsonComponent {
-    codeTs = `@Component({ ... })
+    codeTsCopy = `@Component({ ... })
 export class TestComponent {
     myNewObject?: MyObject;
 
@@ -17,4 +25,47 @@ export class TestComponent {
         this.myNewObject = jsonCopy<MyObject>(myObject);
     }
 }`;
+
+    codeTsParse = `@Component({ ... })
+export class TestComponent {
+    error = '';
+
+    update($event: any) {
+        this.error = '';
+        try {
+            jsonParse($event);
+        } catch (e) {
+            this.error = (e as ExceptionJsonParse).cause;
+        }
+    }
+}`;
+
+    codeTsParseHtml = `<mg-input>
+  <mg-input-label>Json</mg-input-label>
+  <mg-input-textarea [(ngModel)]="json"
+                     (ngModelChange)="update($event)"
+                     autosize
+                     monospace></mg-input-textarea>
+  @if (error) {
+    <mg-input-error>
+      <pre class="mono">{{ error }}</pre>
+    </mg-input-error>
+  }
+</mg-input>`;
+
+    json = `{
+  "text": "example",
+  "array" : [ 1, 2, 3 ]
+}`;
+
+    error = '';
+
+    update($event: any) {
+        this.error = '';
+        try {
+            jsonParse($event);
+        } catch (e) {
+            this.error = (e as ExceptionJsonParse).cause;
+        }
+    }
 }

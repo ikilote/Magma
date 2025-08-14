d29177bfe245f73d5932453d40f07cafa0974498
Zéfling <zefling@ikilote.net>
2025-08-14 23:39:22 +0200

    Message: add type of type

--- projects/ikilote/magma/src/lib/components/message/message-block.component.ts
+++ projects/ikilote/magma/src/lib/components/message/message-block.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 import { MagmaMessageType } from '@ikilote/magma';
 
 @Component({
     selector: 'mg-message-block',
-    templateUrl: './message.component.html',
-    styleUrls: ['./message.component.scss'],
+    templateUrl: './message-block.component.html',
+    styleUrls: ['./message-block.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule],
+    host: {
+        '[class.info]': 'type() === "info"',
+        '[class.success]': 'type() === "success"',
+        '[class.warn]': 'type() === "warn"',
+        '[class.error]': 'type() === "error"',
+        '[class.tip]': 'type() === "tip"',
+    },
 })
 export class MagmaBlockMessage {
-    type = input<MagmaMessageType>();
+    type = input<MagmaMessageType | `${MagmaMessageType}`>();
 }
--- src/app/components/message/demo-message.component.ts
+++ src/app/components/message/demo-message.component.ts
@@ -14,4 +14,24 @@ export class DemoMessageComponent {
   <h1>Test</h1>
   <p>Test</p>
 </mg-message>`;
+    codeHtmlSub = `<mg-message>
+  <mg-message-block>
+    <p>no type</p>
+  </mg-message-block>
+  <mg-message-block type="success">
+    <p>success</p>
+  </mg-message-block>
+  <mg-message-block type="warn">
+    <p>warn</p>
+  </mg-message-block>
+  <mg-message-block type="error">
+    <p>error</p>
+  </mg-message-block>
+  <mg-message-block type="tip">
+    <p>tip</p>
+  </mg-message-block>
+  <mg-message-block type="info">
+    <p>info</p>
+  </mg-message-block>
+</mg-message>`;
 }

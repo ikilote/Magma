fad907dc3181296d9536a04f397fbc8f704c7771
Zéfling <zefling@ikilote.net>
2025-08-15 21:52:57 +0200

    Message: demo refacto

--- src/app/components/message/demo-message.component.ts
+++ src/app/components/message/demo-message.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Json2html, Json2htmlAttr, Json2htmlBody, Json2htmlRef } from '@ikilote/json2html';
+import { Json2html } from '@ikilote/json2html';
 
 import { Select2Data, Select2Option } from 'ng-select2-component';
 
@@ -23,27 +23,7 @@ import { CodeTabsComponent } from '../../demo/code-tabs.component';
 })
 export class DemoMessageComponent {
     codeHtml = '';
-
-    codeHtmlSub = `<mg-message>
-  <mg-message-block>
-    <p>no type</p>
-  </mg-message-block>
-  <mg-message-block type="success">
-    <p>success</p>
-  </mg-message-block>
-  <mg-message-block type="warn">
-    <p>warn</p>
-  </mg-message-block>
-  <mg-message-block type="error">
-    <p>error</p>
-  </mg-message-block>
-  <mg-message-block type="tip">
-    <p>tip</p>
-  </mg-message-block>
-  <mg-message-block type="info">
-    <p>info</p>
-  </mg-message-block>
-</mg-message>`;
+    codeHtmlSub = '';
 
     data: Select2Data = [];
     type: MagmaMessageType = MagmaMessageType.success;
@@ -55,23 +35,29 @@ export class DemoMessageComponent {
         ];
 
         this.codeUpdate();
+
+        this.codeHtmlSub = new Json2html({
+            tag: 'mg-message',
+            attrs: { type: this.type || undefined },
+            body: [undefined, ...enumToValueList(MagmaMessageType)].map(value => ({
+                tag: 'mg-message-block',
+                attrs: { type: value },
+                body: [
+                    { tag: 'h1', body: 'Test', inline: true },
+                    { tag: 'p', body: 'Test', inline: true },
+                ],
+            })),
+        }).toString();
     }
 
     codeUpdate() {
-        const body: Json2htmlBody = [];
-
-        const json: Json2htmlRef = {
+        this.codeHtml = new Json2html({
             tag: 'mg-message',
-            attrs: {
-                type: this.type || undefined,
-            },
+            attrs: { type: this.type || undefined },
             body: [
                 { tag: 'h1', body: 'Test', inline: true },
                 { tag: 'p', body: 'Test', inline: true },
             ],
-        };
-        const attrs: Json2htmlAttr = json.attrs!;
-
-        this.codeHtml = new Json2html(json).toString();
+        }).toString();
     }
 }

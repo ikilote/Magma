35db60ecefa98462a653f09561975c7e91357f87
Zéfling <zefling@ikilote.net>
2025-09-02 00:09:21 +0200

    Draft: contrib-calendar mouths

--- projects/ikilote/magma/src/lib/components/contrib-calendar/contrib-calendar.component.ts
+++ projects/ikilote/magma/src/lib/components/contrib-calendar/contrib-calendar.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { RepeatForPipe } from '../../pipes/repeat-for.pipe';
+
 @Component({
     selector: 'mg-contrib-calendar',
     templateUrl: './contrib-calendar.component.html',
     styleUrls: ['./contrib-calendar.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RepeatForPipe],
 })
 export class MagmaContribCalendar {}

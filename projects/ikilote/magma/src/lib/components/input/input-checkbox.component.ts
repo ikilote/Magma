535c22b9a3220e006ac32a38bd439438ca597549
Zéfling <zefling@ikilote.net>
2026-01-19 22:40:53 +0100

    Demo: add LTR/LTR toggle

--- projects/ikilote/magma/src/lib/components/input/input-checkbox.component.ts
+++ projects/ikilote/magma/src/lib/components/input/input-checkbox.component.ts
@@ -130,7 +130,7 @@ export class MagmaInputCheckbox extends MagmaInputCommon implements DoCheck, Aft
                 .filter(item => item.componentName === this.componentName && (item as MagmaInputCheckbox).testChecked)
                 .map(item => item.value());
         } else {
-            return this.testChecked;
+            return this.host?.forceValue() ? (this.testChecked ? this.value() : null) : this.testChecked;
         }
     }
 }
--- projects/ikilote/magma/src/lib/components/input/input.component.ts
+++ projects/ikilote/magma/src/lib/components/input/input.component.ts
@@ -38,6 +38,7 @@ export class MagmaInput implements OnChanges, AfterContentChecked {
 
     /** for checkbox */
     readonly arrayValue = input(false, { transform: booleanAttribute });
+    readonly forceValue = input(false, { transform: booleanAttribute });
     /** for checkbox & radio */
     readonly alignMode = input<'row' | 'column'>('row');
 
